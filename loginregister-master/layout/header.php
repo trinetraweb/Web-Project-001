ae5fcb006b290f9f89c3c304faaf424a90614625
trinetraweb <84532847+trinetraweb@users.noreply.github.com>
2021-05-21 07:28:06 +0530

    Add files via upload

---
+++ loginregister-master/layout/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title><?=isset($title) ? $title : null;?></title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style/main.css">
+</head>
+<body>
